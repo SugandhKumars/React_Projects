d57de99ac300746ccb9fca6fa77ffb9569fab8b7
SugandhKumars <merrimarrzi@gmail.com>
2024-06-14 12:17:05 +0530

    Converted

--- Currency_Converter/src/App.jsx
+++ Currency_Converter/src/App.jsx
@@ -1,20 +1,65 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [from, setFrom] = useState("");
+  const [to, setTo] = useState("");
+  const [convert, setConvert] = useState("");
+
+  useEffect(() => {
+    if (!from || !to || !input) return;
+    async function fetchCurrency() {
+      const res = await fetch(
+        `https://api.frankfurter.app/latest?amount=${input}&from=${from}&to=${to}`
+      );
+      const data = await res.json();
+      setConvert(data?.rates?.INR);
+    }
+    fetchCurrency();
+  }, [to, from, input]);
   return (
     <>
       <div>Currency Converter</div>
-      <FromCurrency />
-      <ToCurrency />
+      <div className="container">
+        <Input input={input} setInput={setInput} />
+        <FromCurrency from={from} setFrom={setFrom} />
+        <ToCurrency to={to} setTo={setTo} />
+      </div>
+      <p>{convert ? convert : "Result"}</p>
     </>
   );
 }
 
-function FromCurrency() {
+function Input({ input, setInput }) {
+  return (
+    <>
+      <div className="input">
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => {
+            setInput(Number(e.target.value));
+          }}
+        />
+      </div>
+    </>
+  );
+}
+function FromCurrency({ from, setFrom }) {
   return (
     <>
       <div>
-        <select name="" id="">
+        <label htmlFor="">From Currency </label>
+        <select
+          value={from}
+          onChange={(e) => {
+            setFrom(e.target.value);
+          }}
+        >
+          <option value="" disabled>
+            --Select Currency--
+          </option>
           <option value="INR">INR</option>
           <option value="USD">USD</option>
           <option value="EUR">EUR</option>
@@ -23,14 +68,23 @@ function FromCurrency() {
     </>
   );
 }
-function ToCurrency() {
+function ToCurrency({ to, setTo }) {
   return (
     <>
       <div className="main-container">
-        <select name="" id="">
+        <label>To Currency </label>
+        <select
+          value={to}
+          onChange={(e) => {
+            setTo(e.target.value);
+          }}
+        >
+          <option value="" disabled>
+            --Select Currency--
+          </option>
+          <option value="EUR">EUR</option>
           <option value="INR">INR</option>
           <option value="USD">USD</option>
-          <option value="EUR">EUR</option>
         </select>
       </div>
     </>
