d1de3adec9f346524655aaef8f5513cdb226f460
SugandhKumars <merrimarrzi@gmail.com>
2024-07-02 19:14:54 +0530

    split the code

--- worldWise/src/Components/App.jsx
+++ worldWise/src/Components/App.jsx
@@ -1,15 +1,27 @@
-import React, { createContext, useContext, useEffect, useState } from "react";
+import React, {
+  Suspense,
+  createContext,
+  lazy,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Product from "../Pages/Product";
-import Home from "../Pages/Home";
-import Pricing from "../Pages/Pricing";
-import Login from "../Pages/Login";
-import AppLayout from "../Pages/AppLayout";
 import CityList from "./CityList";
 import CountryList from "./CountryList";
 import CityDetails from "./CityDetails";
 import Form from "./Form";
 import "../App.css";
+// import Product from "../Pages/Product";
+// import Home from "../Pages/Home";
+// import Pricing from "../Pages/Pricing";
+// import Login from "../Pages/Login";
+const Product = lazy(() => import("../Pages/Product"));
+const Home = lazy(() => import("../Pages/Home"));
+const Pricing = lazy(() => import("../Pages/Pricing"));
+const Login = lazy(() => import("../Pages/Login"));
+const AppLayout = lazy(() => import("../Pages/AppLayout"));
+// import AppLayout from "../Pages/AppLayout";
 
 // Create Context
 export const CityContext = createContext();
@@ -38,18 +50,20 @@ function App() {
   return (
     <BrowserRouter>
       <CityProvider>
-        <Routes>
-          <Route path="/product" element={<Product />} />
-          <Route path="/" element={<Home />} />
-          <Route path="/pricing" element={<Pricing />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/app" element={<AppLayout />}>
-            <Route path="cities" element={<CityList />} />
-            <Route path="cities/:id" element={<CityDetails />} />
-            <Route path="country" element={<CountryList />} />
-            <Route path="form" element={<Form />} />
-          </Route>
-        </Routes>
+        <Suspense fallback={<h1>Loading....</h1>}>
+          <Routes>
+            <Route path="/product" element={<Product />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/pricing" element={<Pricing />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/app" element={<AppLayout />}>
+              <Route path="cities" element={<CityList />} />
+              <Route path="cities/:id" element={<CityDetails />} />
+              <Route path="country" element={<CountryList />} />
+              <Route path="form" element={<Form />} />
+            </Route>
+          </Routes>
+        </Suspense>
       </CityProvider>
     </BrowserRouter>
   );
